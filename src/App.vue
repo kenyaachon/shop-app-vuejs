e2b208e0f024037790b9b0caf385b3c54a6426c2
mosesmbugua <swahili1963@gmail.com>
2022-09-21 21:57:37 -0700

    CleanUp App

--- src/App.vue
+++ src/App.vue
@@ -25,29 +25,6 @@
   </transition>
 
   <Products :maxPriceFilter="maxPriceFilter" @add="addItem" />
-  <!-- <transition-group
-    name="fade"
-    tag="div"
-    enter-active-class="animate__animated animate__fadeInRight"
-    leave-enter-class="animate__animated animate__fadeOutRight"
-  >
-    <div
-      class="row d-flex mb-3 align-items-center"
-      v-for="clothing of maxPriceFilter"
-      :key="clothing.id"
-    >
-      <div class="col-1 m-auto">
-        <button class="btn btn-info" v-on:click="addItem()">+</button>
-      </div>
-
-      <ClothingItem
-        :name="clothing.name"
-        :description="clothing.description"
-        :price="clothing.price"
-        :image="clothing.image"
-      />
-    </div>
-  </transition-group> -->
 </template>
 
 <script>
