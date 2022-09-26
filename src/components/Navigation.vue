7640bedc3e06b49163903ef906bada85161bdb18
mosesmbugua <swahili1963@gmail.com>
2022-09-25 19:14:56 -0700

    Add linking between Pages

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -43,6 +43,11 @@
               </a>
             </div>
           </div>
+          <router-link
+            class="btn btn-sm btn-outline-info text-dark"
+            to="/checkout"
+            >Checkout</router-link
+          >
         </div>
       </div>
     </div>
